162619a98578e886b9552239cf0b4d6b7a1326cf
Md Jack <mdjac@mail.fenixaubit.com>
2020-04-26 15:29:22 +0530

    add init commit

---
+++ precache-manifest.d794b6220c03aee7d34aadf5bb3c08bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3708587a1fb5d9969beada4c89953294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40b03b0a777548c6bee",
+    "url": "/static/css/main.6301653a.chunk.css"
+  },
+  {
+    "revision": "ad94e6e29233272319b3",
+    "url": "/static/js/2.7076481f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7076481f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40b03b0a777548c6bee",
+    "url": "/static/js/main.c61dd103.chunk.js"
+  },
+  {
+    "revision": "f83ff07250c49a20880c",
+    "url": "/static/js/runtime-main.b74cc8b6.js"
+  }
+]);
